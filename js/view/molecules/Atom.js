478cd63e0f676a016dbcec2fac277f4863f7b8e7
MLearner05 <andrey.zelenkov@mlearner.com>
2014-02-24 16:17:06 +0400

    atom constructor optimization

--- js/view/molecules/Atom.js
+++ js/view/molecules/Atom.js
@@ -24,31 +24,14 @@ define( function( require ) {
 
     // cache gradients for next executions
     if ( !(color in gradients[radius]) ) {
-      gradients[radius][color] = {
-        gradientDefault: new RadialGradient( -radius * 0.2, -radius * 0.3, 0.25, -radius * 0.2, -radius * 0.3, radius * 2 )
-          .addColorStop( 0, 'white' )
-          .addColorStop( 0.33, color )
-          .addColorStop( 1, 'black' ),
-        gradientGray: new RadialGradient( -radius * 0.2, -radius * 0.3, 0.25, -radius * 0.2, -radius * 0.3, radius * 2 )
-          .addColorStop( 0, 'rgb(169,169,169)' )
-          .addColorStop( 0.33, 'rgb(150,150,150)' )
-          .addColorStop( 1, 'rgb(150,150,150)' )
-      };
+      gradients[radius][color] = new RadialGradient( -radius * 0.2, -radius * 0.3, 0.25, -radius * 0.2, -radius * 0.3, radius * 2 )
+        .addColorStop( 0, 'white' )
+        .addColorStop( 0.33, color )
+        .addColorStop( 1, 'black' );
     }
 
-    this.gradientDefault = gradients[radius][color].gradientDefault;
-    this.gradientGray = gradients[radius][color].gradientGray;
-
-    this.view = new Circle( radius, {fill: this.gradientDefault} );
-    this.addChild( this.view );
+    this.addChild( new Circle( radius, {fill: gradients[radius][color]} ) );
   }
 
-  return inherit( Node, Atom, {
-    fillDefault: function() {
-      this.view.setFill( this.gradientDefault );
-    },
-    fillGray: function() {
-      this.view.setFill( this.gradientGray );
-    }
-  } );
+  return inherit( Node, Atom );
 } );
