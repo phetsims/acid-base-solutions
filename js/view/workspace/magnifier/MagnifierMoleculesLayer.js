48e7ef034771aac66ffe26336cc19ee8d39ec96e
MLearner05 <andrey.zelenkov@mlearner.com>
2014-03-07 12:57:04 +0400

    #44 fixed: setMolecules called many times

--- js/view/workspace/magnifier/MagnifierMoleculesLayer.js
+++ js/view/workspace/magnifier/MagnifierMoleculesLayer.js
@@ -32,63 +32,70 @@ define( function( require ) {
   // constants
     MAX_MOLECULES = 50, // TODO: should be 200, but sim will load approximately 30 second
     BASE_DOTS = 2,
-    BASE_CONCENTRATION = 1E-7; // [H3O+] and [OH-] in pure water, value chosen so that pure water shows some molecules;
+    BASE_CONCENTRATION = 1E-7; // [H3O+] and [OH-] in pure water, value chosen so that pure water shows some molecules
 
+  function MagnifierMoleculesLayer( model, boundedSolution, property, type, radius ) {
+    var molecules = [],
+      setMoleculesBinded;
+    Node.call( this );
+    this._pointer = 0; // last shown molecule's index
+
+    // add molecules
+    for ( var i = 0; i < MAX_MOLECULES; i++ ) {
+      this.addChild( molecules[i] = new MoleculesConstructors[type]( {visible: false}, true ) );
+    }
+
+    // update number of molecules only when layer is visible
+    setMoleculesBinded = setMolecules.bind( this, model, boundedSolution, property, molecules );
+    property.lazyLink( setMoleculesBinded );
+    model.property( 'viewMode' ).lazyLink( setMoleculesBinded );
+    model.property( 'solution' ).link( setMoleculesBinded );
+
+    // update position of molecules if solution have been switched
+    model.property( 'solution' ).link( function( newSolution ) {
+      if ( boundedSolution === newSolution ) {
+        updatePosition( molecules, radius );
+      }
+    } );
+  }
+
+  // return number of molecules
   var getNumberOfMolecules = function( concentration ) {
     var raiseFactor = Util.log10( concentration / BASE_CONCENTRATION ),
       baseFactor = Math.pow( ( MAX_MOLECULES / BASE_DOTS ), ( 1 / Util.log10( 1 / BASE_CONCENTRATION ) ) );
     return Math.round( BASE_DOTS * Math.pow( baseFactor, raiseFactor ) );
   };
 
-  function MagnifierMoleculesLayer( model, boundedSolution, property, type, radius ) {
-    var self = this;
-    Node.call( this );
-    this.model = model;
-    this.property = property;
-    this.radius = radius;
-    this.molecules = [];
-    this.pointer = 0; // last shown molecule's index
-
-    // add molecules
-    for ( var i = 0; i < MAX_MOLECULES; i++ ) {
-      this.addChild( this.molecules[i] = new MoleculesConstructors[type]( {visible: false}, true ) );
-    }
-
-    // update layer only when it visible
-    property.link( this.setMolecules.bind( this ) );
-    model.property( 'viewMode' ).link( this.setMolecules.bind( this ) );
-
-    model.property( 'solution' ).link( function( newSolution ) {
-      self.setVisible( boundedSolution === newSolution );
-      if ( boundedSolution === newSolution ) {
-        self.update();
-      }
+  // update position of molecules
+  var updatePosition = function( molecules, radius ) {
+    var r, fi;
+    molecules.forEach( function( molecule ) {
+      r = radius * Math.random();
+      fi = 2 * Math.PI * Math.random();
+      molecule.setTranslation( r * Math.sin( fi ), r * Math.cos( fi ) );
     } );
-  }
+  };
 
-  return inherit( Node, MagnifierMoleculesLayer, {
-    // update position of molecules
-    update: function() {
-      var r, fi, radius = this.radius;
-      this.molecules.forEach( function( molecule ) {
-        r = radius * Math.random();
-        fi = 2 * Math.PI * Math.random();
-        molecule.setTranslation( r * Math.sin( fi ), r * Math.cos( fi ) );
-      } );
-    },
-    setMolecules: function() {
-      var numberOfMolecules = getNumberOfMolecules( this.property.get() ),
-        pointer = this.pointer,
-        visibility,
-        i;
+  // show appropriate number of molecules
+  var setMolecules = function( model, boundedSolution, property, molecules ) {
+    var numberOfMolecules,
+      visibility,
+      i;
 
-      // show appropriate number of molecules
-      if ( numberOfMolecules !== pointer && this.model.viewMode === ViewModes.MOLECULES ) {
-        for ( i = Math.min( pointer, numberOfMolecules ), visibility = (numberOfMolecules > pointer); i < Math.max( pointer, numberOfMolecules ); i++ ) {
-          this.molecules[i].setVisible( visibility );
+    // update visibility of layer
+    this.setVisible( model.solution === boundedSolution && model.viewMode === ViewModes.MOLECULES );
+
+    // update number of molecules only when layer is visible
+    if ( this.visible ) {
+      numberOfMolecules = getNumberOfMolecules( property.get() );
+      if ( numberOfMolecules !== this._pointer ) {
+        for ( i = Math.min( this._pointer, numberOfMolecules ), visibility = (numberOfMolecules > this._pointer); i < Math.max( this._pointer, numberOfMolecules ); i++ ) {
+          molecules[i].setVisible( visibility );
         }
-        this.pointer = numberOfMolecules;
+        this._pointer = numberOfMolecules;
       }
     }
-  } );
+  };
+
+  return inherit( Node, MagnifierMoleculesLayer );
 } );
