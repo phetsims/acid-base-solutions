4e18dfad58bb77a6af73ec8182e3a730bbbbc2a1
Chris Malley <cmalley@pixelzoom.com>
2014-03-21 18:00:21 -0600

    jsdoc

--- js/model/SolutionsMenuModel.js
+++ js/model/SolutionsMenuModel.js
@@ -1,7 +1,7 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 /**
- * Model for the solutions control panel in 'Acid-Base Solutions' sim.
+ * Model for the 'Solutions' control panel in 'Acid-Base Solutions' sim.
  *
  * @author Andrey Zelenkov (Mlearner)
  */
