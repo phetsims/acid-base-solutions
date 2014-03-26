0d8faba1a99dda79fdd04ee8dea5bcd829753cec
Chris Malley <cmalley@pixelzoom.com>
2014-03-25 21:59:00 -0600

    #51 switch to sun.InOutRadioButton for 'Tools' panel

--- js/common/view/ToolsPanel.js
+++ js/common/view/ToolsPanel.js
@@ -11,20 +11,36 @@ define( function( require ) {
   'use strict';
 
   // imports
-  var AquaRadioButton = require( 'SUN/AquaRadioButton' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var HStrut = require( 'SUN/HStrut' );
   var Image = require( 'SCENERY/nodes/Image' );
+  var InOutRadioButton = require( 'SUN/InOutRadioButton' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var ToolMode = require( 'ACID_BASE_SOLUTIONS/common/enum/ToolMode' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
+  var VStrut = require( 'SUN/VStrut' );
 
   // images
-  var lightBulbImage = require( 'image!ACID_BASE_SOLUTIONS/light-bulb.png' );
-  var pHMeterImage = require( 'image!ACID_BASE_SOLUTIONS/pH-meter.png' );
-  var pHPaperImage = require( 'image!ACID_BASE_SOLUTIONS/pH-paper.png' );
+  var lightBulbImage = require( 'image!ACID_BASE_SOLUTIONS/light-bulb-icon.png' );
+  var pHMeterImage = require( 'image!ACID_BASE_SOLUTIONS/pH-meter-icon.png' );
+  var pHPaperImage = require( 'image!ACID_BASE_SOLUTIONS/pH-paper-icon.png' );
 
   // constants
+  var MIN_BUTTON_WIDTH = 40;
   var ICON_OPTIONS = { scale: 0.75 };
-  var RADIO_BUTTON_OPTIONS = { radius: 7 };
+  var RADIO_BUTTON_OPTIONS = {
+    cornerRadius: 8,
+    shadowFill: 'rgba(0,0,0,0.5)'
+  };
+
+  // Creates a radio-button icons that has a min size
+  var createIcon = function( imageNode, minWidth, minHeight ) {
+    var hStrut = new HStrut( minWidth );
+    var vStrut = new VStrut( minHeight );
+    imageNode.centerX = hStrut.centerX;
+    imageNode.centerY = vStrut.centerY;
+    return new Node( { children: [ hStrut, vStrut, imageNode ] } );
+  };
 
   /**
    * @param {Property<ToolMode>} toolModeProperty
@@ -38,14 +54,22 @@ define( function( require ) {
       align: 'left'
     }, options );
 
+    var pHMeterNode = new Image( pHMeterImage, ICON_OPTIONS );
+    var pHPaperNode = new Image( pHPaperImage, ICON_OPTIONS );
+    var lightBulbNode = new Image( lightBulbImage, ICON_OPTIONS );
+
+    // determine uniform size for buttons, in case icons have different sizes
+    var minWidth = Math.max( MIN_BUTTON_WIDTH, Math.max( pHMeterNode.width, Math.max( pHPaperNode.width, lightBulbNode.width ) ) );
+    var minHeight = Math.max( pHMeterNode.height, Math.max( pHPaperNode.height, lightBulbNode.height ) );
+
     options.children = [
-      new AquaRadioButton( toolModeProperty, ToolMode.PH_METER, new Image( pHMeterImage, ICON_OPTIONS ), RADIO_BUTTON_OPTIONS ),
-      new AquaRadioButton( toolModeProperty, ToolMode.PH_PAPER, new Image( pHPaperImage, ICON_OPTIONS ), RADIO_BUTTON_OPTIONS ),
-      new AquaRadioButton( toolModeProperty, ToolMode.CONDUCTIVITY, new Image( lightBulbImage, ICON_OPTIONS ), RADIO_BUTTON_OPTIONS )
+      new InOutRadioButton( toolModeProperty, ToolMode.PH_METER, createIcon( pHMeterNode, minWidth, minHeight ), RADIO_BUTTON_OPTIONS ),
+      new InOutRadioButton( toolModeProperty, ToolMode.PH_PAPER, createIcon( pHPaperNode, minWidth, minHeight ), RADIO_BUTTON_OPTIONS ),
+      new InOutRadioButton( toolModeProperty, ToolMode.CONDUCTIVITY, createIcon( lightBulbNode, minWidth, minHeight ), RADIO_BUTTON_OPTIONS )
     ];
 
-    VBox.call( this, options );
+    HBox.call( this, options );
   }
 
-  return inherit( VBox, ToolsPanel );
+  return inherit( HBox, ToolsPanel );
 } );
