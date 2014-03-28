44074f461722d5784e9f6ec6f1abd8c5190a7de7
Chris Malley <cmalley@pixelzoom.com>
2014-03-27 23:17:16 -0600

    clean up

--- js/common/ABSConstants.js
+++ js/common/ABSConstants.js
@@ -13,15 +13,14 @@ define( function( require ) {
   var Range = require( 'DOT/Range' );
 
   // constants
-  var WEAK_STRENGTH_RANGE = new Range( 1E-10, 1E2, 1E-7 ),
-    STRONG_STRENGTH = WEAK_STRENGTH_RANGE.max + 1; // arbitrary, but needs to be greater than weak range
+  var WEAK_STRENGTH_MAX = 1E2;
 
   return {
     CONCENTRATION_RANGE: new Range( 1E-3, 1, 1E-2 ),
     PH_RANGE: new Range( 0, 14 ),
-    STRONG_STRENGTH: STRONG_STRENGTH,
     WATER_EQUILIBRIUM_CONSTANT: 1E-14,
     WATER_CONCENTRATION: 55.6, // water concentration when it's used as a solvent, mol/L
-    WEAK_STRENGTH_RANGE: WEAK_STRENGTH_RANGE
+    WEAK_STRENGTH_RANGE: new Range( 1E-10, WEAK_STRENGTH_MAX, 1E-7 ),
+    STRONG_STRENGTH: WEAK_STRENGTH_MAX + 1 // arbitrary, but needs to be greater than weak max
   };
 } );
