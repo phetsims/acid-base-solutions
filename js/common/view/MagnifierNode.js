d38d83c5714809241d2450027feea02114c404ba
Chris Malley <cmalley@pixelzoom.com>
2014-04-01 15:26:04 -0600

    #5 a bit of work on new MagnifierNode

--- js/common/view/MagnifierNode.js
+++ js/common/view/MagnifierNode.js
@@ -15,6 +15,7 @@ define( function( require ) {
   var CanvasNode = require( 'SCENERY/nodes/CanvasNode' );
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var MoleculeFactory = require( 'ACID_BASE_SOLUTIONS/common/view/MoleculeFactory' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
@@ -24,8 +25,48 @@ define( function( require ) {
   var solventImage = require( 'image!ACID_BASE_SOLUTIONS/../images/solvent.png' );
 
   function MoleculesNode( lensBounds ) {
-    //TODO generate an image for each molecule type, or get from model?
+
     CanvasNode.call( this, { canvasBounds: lensBounds } );
+
+    //TODO are all of these necessary? iterate over solutions to find which ones are needed?
+    //@private images for each type of molecule
+    this.moleculeImages = {
+      A: null,
+      B: null,
+      BH: null,
+      HA: null,
+      H3O: null,
+      M: null,
+      MOH: null,
+      OH: null
+    };
+
+    //TODO replace this with iteration over this.moleculeImages properties
+    // generate images, this happens asynchronously
+    MoleculeFactory.A().toImage( function( image, x, y ) {
+      this.moleculeImages.A = image;
+    } );
+    MoleculeFactory.B().toImage( function( image, x, y ) {
+      this.moleculeImages.B = image;
+    } );
+    MoleculeFactory.BH().toImage( function( image, x, y ) {
+      this.moleculeImages.BH = image;
+    } );
+    MoleculeFactory.HA().toImage( function( image, x, y ) {
+      this.moleculeImages.HA = image;
+    } );
+    MoleculeFactory.H3O().toImage( function( image, x, y ) {
+      this.moleculeImages.H3O = image;
+    } );
+    MoleculeFactory.M().toImage( function( image, x, y ) {
+      this.moleculeImages.M = image;
+    } );
+    MoleculeFactory.MOH().toImage( function( image, x, y ) {
+      this.moleculeImages.MOH = image;
+    } );
+    MoleculeFactory.OH().toImage( function( image, x, y ) {
+      this.moleculeImages.OH = image;
+    } );
   }
 
   inherit( CanvasNode, MoleculesNode, {
@@ -38,6 +79,23 @@ define( function( require ) {
     paintCanvas: function( wrapper ) {
       //TODO draw using context.drawImage
       console.log( 'MoleculesNode.paintCanvas' );//XXX
+    },
+
+    /**
+     * Paints one species of molecule. Using drawImage is faster than arc.
+     * @private
+     * @param {CanvasContextWrapper} wrapper
+     * @param {Number} numberOfMolecules
+     * @param {Image} image
+     * @param {Array<Number>} xCoords
+     * @param {Array<Number>} yCoords
+     */
+    paintMolecules: function( wrapper, numberOfMolecules, image, xCoords, yCoords ) {
+      if ( image ) { // images are generated asynchronously, so test just in case they aren't available when this is first called
+        for ( var i = 0; i < numberOfMolecules; i++ ) {
+          wrapper.context.drawImage( image, xCoords[i], yCoords[i] );
+        }
+      }
     }
   } );
 
@@ -45,12 +103,12 @@ define( function( require ) {
    * @param {MagnifierModel} magnifierModel
    * @constructor
    */
-  function MagnifierNode( magnifierModel ) {
+  function MagnifierNode( magnifier ) {
 
     var self = this;
     Node.call( this );
 
-    var radius = magnifierModel.radius;
+    var radius = magnifier.radius;
 
     // lens
     var lensShape = Shape.circle( 0, 0, radius );
@@ -64,7 +122,7 @@ define( function( require ) {
     this.solventNode = new Image( solventImage, { scale: 0.5, x: -radius * Math.SQRT2, y: -radius * Math.SQRT2 } );
 
     // molecules
-    var moleculesNode = new MoleculesNode( new Bounds2( magnifierModel.location.x - radius, magnifierModel.location.y - radius, magnifierModel.location.x + radius, magnifierModel.location.y + radius ) );
+    var moleculesNode = new MoleculesNode( new Bounds2( magnifier.location.x - radius, magnifier.location.y - radius, magnifier.location.x + radius, magnifier.location.y + radius ) );
     moleculesNode.clipArea = lensShape;
 
     // rendering order
@@ -73,9 +131,12 @@ define( function( require ) {
     this.addChild( handleNode );
     this.addChild( lensNode );
 
-    this.translation = magnifierModel.location;
+    this.translation = magnifier.location;
 
-    //TODO when solutionType changes, re-wire to concentration properties for each molecule in the solution
+    // when solutionType changes, re-wire to new solution
+    magnifier.solutionTypeProperty.link( function( newSolutionType, previousSolutionType ) {
+      //TODO
+    } );
   }
 
   return inherit( Node, MagnifierNode, {
