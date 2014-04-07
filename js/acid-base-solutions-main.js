9ee917e9630ef366957ce603810eed215566694b
Chris Malley <cmalley@pixelzoom.com>
2014-04-07 16:02:19 -0600

    #96 create the 'Custom Solution' screen icon programmatically, so that molecule colors are correct

--- js/acid-base-solutions-main.js
+++ js/acid-base-solutions-main.js
@@ -16,12 +16,14 @@ define( function( require ) {
   var Image = require( 'SCENERY/nodes/Image' );
   var IntroductionModel = require( 'ACID_BASE_SOLUTIONS/introduction/model/IntroductionModel' );
   var IntroductionView = require( 'ACID_BASE_SOLUTIONS/introduction/view/IntroductionView' );
+  var MoleculeFactory = require( 'ACID_BASE_SOLUTIONS/common/view/MoleculeFactory' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Screen = require( 'JOIST/Screen' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
 
   // images
-  var customSolutionIcon = require( 'image!ACID_BASE_SOLUTIONS/custom-solution-icon.png' );
   var introductionIcon = require( 'image!ACID_BASE_SOLUTIONS/introduction-icon.png' );
 
   // strings
@@ -29,13 +31,41 @@ define( function( require ) {
   var introductionTitleString = require( 'string!ACID_BASE_SOLUTIONS/introductionTitle' );
   var simTitleString = require( 'string!ACID_BASE_SOLUTIONS/acid-base-solutions.name' );
 
+  /**
+   * Create the icon for the 'Custom Solution' screen.
+   * @param width
+   * @param height
+   * @returns {Node}
+   */
+  var createCustomSolutionsIcon = function( width, height ) {
+
+    var h3oNode = MoleculeFactory.H3O();
+    var ohNode = MoleculeFactory.OH();
+
+    // Uniformly scale the molecules to make them fill the available space.
+    var xSpace = 0.06 * width; // horizontal space around the molecules
+    var maxWidth = Math.max( h3oNode.width, ohNode.width );
+    var moleculeScale = 0.5 * ( width - 3 * xSpace ) / maxWidth;
+    h3oNode.setScaleMagnitude( moleculeScale, moleculeScale );
+    ohNode.setScaleMagnitude( moleculeScale, moleculeScale );
+
+    // position the molecules
+    h3oNode.left = xSpace;
+    ohNode.right = width - xSpace;
+    h3oNode.centerY = ohNode.centerY = height / 2;
+
+    var background = new Rectangle( 0, 0, width, height, { fill: 'white' } );
+
+    return new Node( { children: [ background, h3oNode, ohNode ] } );
+  };
+
   var screens = [
     new Screen( introductionTitleString, new Image( introductionIcon ),
       function() { return new IntroductionModel(); },
       function( model ) { return new IntroductionView( model ); },
       { backgroundColor: ABSColors.SCREEN_BACKGROUND }
     ),
-    new Screen( customSolutionTitleString, new Image( customSolutionIcon ),
+    new Screen( customSolutionTitleString, createCustomSolutionsIcon( 548, 373 ),
       function() { return new CustomSolutionModel(); },
       function( model ) { return new CustomSolutionView( model ); },
       { backgroundColor: ABSColors.SCREEN_BACKGROUND }
