9caabd63491563d8abec36c2b6b97bb05b20e623
Chris Malley <cmalley@pixelzoom.com>
2015-02-11 18:39:20 -0700

    move requirejs plugins to chipper/js/requirejs-plugins, phetsims/chipper#92

--- js/acid-base-solutions-config.js
+++ js/acid-base-solutions-config.js
@@ -13,8 +13,8 @@ require.config( {
   paths: {
 
     // plugins
-    image: '../../chipper/requirejs-plugins/image',
-    string: '../../chipper/requirejs-plugins/string',
+    image: '../../chipper/js/requirejs-plugins/image',
+    string: '../../chipper/js/requirejs-plugins/string',
 
     text: '../../sherpa/text',
 
