c5f0eb0745e877c6a3a92ee5d3d5880ae652ab8c
samreid <reids@colorado.edu>
2015-11-06 20:50:43 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/common/model/Beaker.js
+++ js/common/model/Beaker.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Model for the beaker in 'Acid-Base Solutions' sim.
--- js/common/view/BeakerNode.js
+++ js/common/view/BeakerNode.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Visual representation of a beaker that is filled to the top with a solution.
