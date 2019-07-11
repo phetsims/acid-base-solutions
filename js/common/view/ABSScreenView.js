e652cad11537b90b9675c4761f82b0135a73bda4
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 12:53:50 -0800

    update copyright dates, https://github.com/phetsims/tasks/issues/988

--- js/common/view/ABSScreenView.js
+++ js/common/view/ABSScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2018, University of Colorado Boulder
+// Copyright 2017-2019, University of Colorado Boulder
 
 /**
  * Base type for views in 'Acid-Base Solutions' sim.
--- js/common/view/MoleculeFactory.js
+++ js/common/view/MoleculeFactory.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2015, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * Factory for creating molecule nodes.
