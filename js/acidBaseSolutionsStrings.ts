cdf80e8d46bbd59001e7cadbc1cff64e55f3948e
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:39 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/acidBaseSolutionsStrings.ts
@@ -0,0 +1,59 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import acidBaseSolutions from './acidBaseSolutions.js';
+
+type StringsType = {
+  'acid': string,
+  'acid-base-solutions': {
+    'title': string
+  },
+  'base': string,
+  'concentrationGraph': {
+    'yAxis': string
+  },
+  'screen': {
+    'mySolution': string,
+    'introduction': string
+  },
+  'graph': string,
+  'hideViews': string,
+  'initialConcentration': string,
+  'liters': string,
+  'molecules': string,
+  'negligible': string,
+  'pattern': {
+    '0value': {
+      '1power': string,
+      '1units': string
+    },
+    '0label': {
+      '1value': string
+    }
+  },
+  'pH': string,
+  'pHColorKey': string,
+  'solution': string,
+  'solvent': string,
+  'strength': string,
+  'strong': string,
+  'strongAcid': string,
+  'strongBase': string,
+  'stronger': string,
+  'views': string,
+  'water': string,
+  'weak': string,
+  'weakAcid': string,
+  'weakBase': string,
+  'weaker': string
+};
+
+const acidBaseSolutionsStrings = getStringModule( 'ACID_BASE_SOLUTIONS' ) as StringsType;
+
+acidBaseSolutions.register( 'acidBaseSolutionsStrings', acidBaseSolutionsStrings );
+
+export default acidBaseSolutionsStrings;
