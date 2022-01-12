efba13e7bc957d435543fcf28f2e15e651c46968
phet-dev <phetadmin@gmail.com>
2022-01-12 03:17:39 -0700

    update copyright dates from daily grunt work

--- js/common/view/ToolsRadioButtonGroup.js
+++ js/common/view/ToolsRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Radio buttons for selecting between a set of mutually-exclusive tools.
