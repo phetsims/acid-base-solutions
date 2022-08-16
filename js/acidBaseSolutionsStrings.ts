e0676e0d2fc6c442fad3a0a932e5ce2c783885e0
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:36 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/acidBaseSolutionsStrings.ts
+++ js/acidBaseSolutionsStrings.ts
@@ -5,51 +5,82 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import acidBaseSolutions from './acidBaseSolutions.js';
 
 type StringsType = {
   'acid': string;
+  'acidProperty': TReadOnlyProperty<string>;
   'acid-base-solutions': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'base': string;
+  'baseProperty': TReadOnlyProperty<string>;
   'concentrationGraph': {
     'yAxis': string;
+    'yAxisProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'mySolution': string;
+    'mySolutionProperty': TReadOnlyProperty<string>;
     'introduction': string;
+    'introductionProperty': TReadOnlyProperty<string>;
   };
   'graph': string;
+  'graphProperty': TReadOnlyProperty<string>;
   'hideViews': string;
+  'hideViewsProperty': TReadOnlyProperty<string>;
   'initialConcentration': string;
+  'initialConcentrationProperty': TReadOnlyProperty<string>;
   'liters': string;
+  'litersProperty': TReadOnlyProperty<string>;
   'molecules': string;
+  'moleculesProperty': TReadOnlyProperty<string>;
   'negligible': string;
+  'negligibleProperty': TReadOnlyProperty<string>;
   'pattern': {
     '0value': {
       '1power': string;
+      '1powerProperty': TReadOnlyProperty<string>;
       '1units': string;
+      '1unitsProperty': TReadOnlyProperty<string>;
     };
     '0label': {
       '1value': string;
+      '1valueProperty': TReadOnlyProperty<string>;
     }
   };
   'pH': string;
+  'pHProperty': TReadOnlyProperty<string>;
   'pHColorKey': string;
+  'pHColorKeyProperty': TReadOnlyProperty<string>;
   'solution': string;
+  'solutionProperty': TReadOnlyProperty<string>;
   'solvent': string;
+  'solventProperty': TReadOnlyProperty<string>;
   'strength': string;
+  'strengthProperty': TReadOnlyProperty<string>;
   'strong': string;
+  'strongProperty': TReadOnlyProperty<string>;
   'strongAcid': string;
+  'strongAcidProperty': TReadOnlyProperty<string>;
   'strongBase': string;
+  'strongBaseProperty': TReadOnlyProperty<string>;
   'stronger': string;
+  'strongerProperty': TReadOnlyProperty<string>;
   'views': string;
+  'viewsProperty': TReadOnlyProperty<string>;
   'water': string;
+  'waterProperty': TReadOnlyProperty<string>;
   'weak': string;
+  'weakProperty': TReadOnlyProperty<string>;
   'weakAcid': string;
+  'weakAcidProperty': TReadOnlyProperty<string>;
   'weakBase': string;
+  'weakBaseProperty': TReadOnlyProperty<string>;
   'weaker': string;
+  'weakerProperty': TReadOnlyProperty<string>;
 };
 
 const acidBaseSolutionsStrings = getStringModule( 'ACID_BASE_SOLUTIONS' ) as StringsType;
