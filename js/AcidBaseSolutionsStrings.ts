7879c04675ae58a91201f3df6be10a838f90a1c4
pixelzoom <cmalley@pixelzoom.com>
2022-10-06 15:52:35 -0600

    "supportsDynamicLocale": true

--- js/AcidBaseSolutionsStrings.ts
+++ js/AcidBaseSolutionsStrings.ts
@@ -9,75 +9,46 @@ import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import acidBaseSolutions from './acidBaseSolutions.js';
 
 type StringsType = {
-  'acid': string;
   'acidStringProperty': LinkableProperty<string>;
   'acid-base-solutions': {
-    'title': string;
     'titleStringProperty': LinkableProperty<string>;
   };
-  'base': string;
   'baseStringProperty': LinkableProperty<string>;
   'concentrationGraph': {
-    'yAxis': string;
     'yAxisStringProperty': LinkableProperty<string>;
   };
   'screen': {
-    'mySolution': string;
     'mySolutionStringProperty': LinkableProperty<string>;
-    'introduction': string;
     'introductionStringProperty': LinkableProperty<string>;
   };
-  'graph': string;
   'graphStringProperty': LinkableProperty<string>;
-  'hideViews': string;
   'hideViewsStringProperty': LinkableProperty<string>;
-  'initialConcentration': string;
   'initialConcentrationStringProperty': LinkableProperty<string>;
-  'liters': string;
   'litersStringProperty': LinkableProperty<string>;
-  'molecules': string;
   'moleculesStringProperty': LinkableProperty<string>;
-  'negligible': string;
   'negligibleStringProperty': LinkableProperty<string>;
   'pattern': {
     '0value': {
-      '1units': string;
       '1unitsStringProperty': LinkableProperty<string>;
     };
     '0label': {
-      '1value': string;
       '1valueStringProperty': LinkableProperty<string>;
     }
   };
-  'pH': string;
   'pHStringProperty': LinkableProperty<string>;
-  'pHColorKey': string;
   'pHColorKeyStringProperty': LinkableProperty<string>;
-  'solution': string;
   'solutionStringProperty': LinkableProperty<string>;
-  'solvent': string;
   'solventStringProperty': LinkableProperty<string>;
-  'strength': string;
   'strengthStringProperty': LinkableProperty<string>;
-  'strong': string;
   'strongStringProperty': LinkableProperty<string>;
-  'strongAcid': string;
   'strongAcidStringProperty': LinkableProperty<string>;
-  'strongBase': string;
   'strongBaseStringProperty': LinkableProperty<string>;
-  'stronger': string;
   'strongerStringProperty': LinkableProperty<string>;
-  'views': string;
   'viewsStringProperty': LinkableProperty<string>;
-  'water': string;
   'waterStringProperty': LinkableProperty<string>;
-  'weak': string;
   'weakStringProperty': LinkableProperty<string>;
-  'weakAcid': string;
   'weakAcidStringProperty': LinkableProperty<string>;
-  'weakBase': string;
   'weakBaseStringProperty': LinkableProperty<string>;
-  'weaker': string;
   'weakerStringProperty': LinkableProperty<string>;
 };
 
