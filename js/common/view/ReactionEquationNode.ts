84cbcd580fe515388a2da5b11944c40ee479f575
pixelzoom <cmalley@pixelzoom.com>
2022-11-02 15:49:55 -0600

    convert to TypeScript

--- js/common/view/ReactionEquationNode.ts
+++ js/common/view/ReactionEquationNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2021, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * This node displays the reaction equation that appears below the beaker.
  * It displays the equation that corresponds to the selected solution type.
@@ -11,59 +10,58 @@
 import { Node } from '../../../../scenery/js/imports.js';
 import acidBaseSolutions from '../../acidBaseSolutions.js';
 import ReactionEquationFactory from './ReactionEquationFactory.js';
+import Beaker from '../model/Beaker.js';
+import { SolutionType } from '../enum/SolutionType.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 
-class ReactionEquationNode extends Node {
+export default class ReactionEquationNode extends Node {
 
-  /**
-   * @param {Beaker} beaker
-   * @param {Property.<SolutionType>} solutionTypeProperty
-   */
-  constructor( beaker, solutionTypeProperty ) {
+  public constructor( beaker: Beaker, solutionTypeProperty: TReadOnlyProperty<SolutionType> ) {
 
     super();
 
     // create a Map, so we can look up equations by SolutionType
-    const equations = new Map();
-    equations.set( 'water', ReactionEquationFactory.createWaterEquation() );
-    equations.set( 'strongAcid', ReactionEquationFactory.createStrongAcidEquation() );
-    equations.set( 'weakAcid', ReactionEquationFactory.createWeakAcidEquation() );
-    equations.set( 'strongBase', ReactionEquationFactory.createStrongBaseEquation() );
-    equations.set( 'weakBase', ReactionEquationFactory.createWeakBaseEquation() );
+    const equationsMap = new Map<SolutionType, Node>();
+    equationsMap.set( 'water', ReactionEquationFactory.createWaterEquation() );
+    equationsMap.set( 'strongAcid', ReactionEquationFactory.createStrongAcidEquation() );
+    equationsMap.set( 'weakAcid', ReactionEquationFactory.createWeakAcidEquation() );
+    equationsMap.set( 'strongBase', ReactionEquationFactory.createStrongBaseEquation() );
+    equationsMap.set( 'weakBase', ReactionEquationFactory.createWeakBaseEquation() );
 
     // find max width of equations
-    const maxWidth = getMaxWidth( equations );
+    const maxWidth = getMaxWidth( equationsMap );
 
-    // add equations with central alignment
-    for ( const equation in equations ) {
-      equations[ equation ].setX( ( maxWidth - equations[ equation ].getWidth() ) / 2 );
-      equations[ equation ].setVisible( false );
-      this.addChild( equations[ equation ] );
-    }
+    // Add all equations, initially invisible.
+    equationsMap.forEach( ( node, solutionType ) => {
+      node.visible = false;
+      this.addChild( node );
+    } );
 
-    // position below the beaker
-    this.translation = beaker.position.plusXY( -maxWidth / 2, 10 );
+    // center below the beaker
+    this.boundsProperty.link( bounds => {
+      this.translation = beaker.position.plusXY( -maxWidth / 2, 10 );
+    } );
 
     // add observer for equations
     solutionTypeProperty.link( ( newSolutionType, prevSolutionType ) => {
 
       // hide previous equation
       if ( prevSolutionType ) {
-        equations.get( prevSolutionType ).setVisible( false );
+        equationsMap.get( prevSolutionType )!.setVisible( false );
       }
 
       // show new equation
-      equations.get( newSolutionType ).setVisible( true );
+      equationsMap.get( newSolutionType )!.setVisible( true );
     } );
   }
 }
 
-function getMaxWidth( equations ) {
+function getMaxWidth( equationsMap: Map<SolutionType, Node> ): number {
   let maxWidth = 0;
-  for ( const equation in equations ) {
-    maxWidth = Math.max( maxWidth, equations[ equation ].getWidth() );
-  }
+  equationsMap.forEach( ( node, solutionType ) => {
+    maxWidth = Math.max( maxWidth, node.width );
+  } );
   return maxWidth;
 }
 
 acidBaseSolutions.register( 'ReactionEquationNode', ReactionEquationNode );
-export default ReactionEquationNode;
