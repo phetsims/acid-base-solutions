484cd7c1cf0f8c2f0b447d1ccb7ce54131df47ad
pixelzoom <cmalley@pixelzoom.com>
2022-11-02 14:02:58 -0600

    // @ts-nocheck

--- js/introduction/view/IntroductionSolutionPanel.js
+++ js/introduction/view/IntroductionSolutionPanel.js
@@ -1,5 +1,6 @@
 // Copyright 2014-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * IntroductionSolutionPanel is a panel for selecting between a set of mutually-exclusive solutions in the
  * Introduction screen.
