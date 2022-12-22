701665ef38a77889d4171d7c383d8ff6bc70864a
pixelzoom <cmalley@pixelzoom.com>
2022-12-22 16:42:43 -0700

    use DynamicProperty for mapping between log and linear scales, https://github.com/phetsims/acid-base-solutions/issues/167

--- js/mysolution/view/InitialConcentrationSlider.ts
+++ js/mysolution/view/InitialConcentrationSlider.ts
@@ -8,9 +8,10 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import DynamicProperty from '../../../../axon/js/DynamicProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
+import Range from '../../../../dot/js/Range.js';
 import RangeWithValue from '../../../../dot/js/RangeWithValue.js';
 import Utils from '../../../../dot/js/Utils.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
@@ -23,9 +24,18 @@ export default class InitialConcentrationSlider extends HSlider {
 
   public constructor( concentrationProperty: Property<number>, concentrationRange: RangeWithValue, tandem: Tandem ) {
 
-    const model = new SliderModel( concentrationProperty, concentrationRange );
+    // Property that maps between concentration (log) and sliderValue (linear)
+    const sliderValueProperty = new DynamicProperty( new Property( concentrationProperty ), {
+      bidirectional: true,
+      reentrant: true, // Necessary because bidirectional:true
+      map: ( concentration: number ) => logToLinear( concentration ),
+      inverseMap: ( sliderValue: number ) => linearToLog( sliderValue )
+    } );
 
-    super( model.sliderValueProperty, model.sliderValueRange, {
+    // Convert range from concentration (log)  to sliderValue (linear)
+    const sliderValueRange = new Range( logToLinear( concentrationRange.min ), logToLinear( concentrationRange.max ) );
+
+    super( sliderValueProperty, sliderValueRange, {
       trackSize: new Dimension2( 125, 4 ),
       thumbSize: new Dimension2( 12, 24 ),
       thumbTouchAreaXDilation: 6,
@@ -35,10 +45,12 @@ export default class InitialConcentrationSlider extends HSlider {
       tandem: tandem
     } );
 
-    // add labels tick marks
+    // Tick marks
+    //TODO this is hardcoded to a specific range
+    assert && assert( concentrationRange.min === 0.001 && concentrationRange.max === 1 );
     const numberOfTicks = 4;
-    for ( let i = 0, step = model.sliderValueRange.getLength() / ( numberOfTicks - 1 ); i < numberOfTicks; i++ ) {
-      this.addMajorTick( model.sliderValueRange.min + step * i, new Text( concentrationRange.min * Math.pow( 10, i ), { font: new PhetFont( 10 ) } ) );
+    for ( let i = 0, step = sliderValueRange.getLength() / ( numberOfTicks - 1 ); i < numberOfTicks; i++ ) {
+      this.addMajorTick( sliderValueRange.min + step * i, new Text( concentrationRange.min * Math.pow( 10, i ), { font: new PhetFont( 10 ) } ) );
     }
   }
 
@@ -48,43 +60,12 @@ export default class InitialConcentrationSlider extends HSlider {
   }
 }
 
-/**
- * Model for the concentration slider.
- * Maps between the linear slider and the logarithmic range of concentration.
- * Implemented as an inner type because this is internal to the slider.
- */
-class SliderModel {
+function logToLinear( value: number ): number {
+  return Utils.log10( value );
+}
 
-  private readonly concentrationProperty: Property<number>;
-  public readonly sliderValueRange: RangeWithValue;
-  public readonly sliderValueProperty: Property<number>;
-
-  public constructor( concentrationProperty: Property<number>, concentrationRange: RangeWithValue ) {
-
-    this.concentrationProperty = concentrationProperty;
-
-    this.sliderValueRange = new RangeWithValue(
-      Utils.log10( concentrationRange.min ),
-      Utils.log10( concentrationRange.max ),
-      Utils.log10( concentrationRange.defaultValue ) );
-
-    this.sliderValueProperty = new NumberProperty( Utils.log10( concentrationProperty.value ), {
-      reentrant: true
-    } );
-
-    // map between linear and logarithmic
-    this.sliderValueProperty.link( sliderValue => {
-      // See https://github.com/phetsims/acid-base-solutions/issues/73
-      this.concentrationProperty.value = Utils.toFixedNumber( Math.pow( 10, sliderValue ), 10 );
-    } );
-    concentrationProperty.link( concentration => {
-      this.sliderValueProperty.value = Utils.log10( concentration );
-    } );
-  }
-
-  public dispose(): void {
-    assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
-  }
+function linearToLog( value: number ): number {
+  return Utils.toFixedNumber( Math.pow( 10, value ), 10 );
 }
 
 acidBaseSolutions.register( 'InitialConcentrationSlider', InitialConcentrationSlider );
