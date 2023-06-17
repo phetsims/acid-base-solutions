d229894fcc1d1ee5c3dd5dfcf4ca59873496c01d
phet-dev <phetadmin@gmail.com>
2023-06-17 03:27:37 -0600

    update copyright dates from daily grunt work

--- js/common/view/ConcentrationBarNode.ts
+++ js/common/view/ConcentrationBarNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * ConcentrationBarNode is a bar in the 'Equilibrium Concentration' graph.
