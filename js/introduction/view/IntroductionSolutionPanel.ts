5ae03fe4c10088f4b4385db7ee4710a2e75b7ce7
phet-dev <phetadmin@gmail.com>
2023-07-26 03:27:52 -0600

    update copyright dates from daily grunt work

--- js/introduction/view/IntroductionSolutionPanel.ts
+++ js/introduction/view/IntroductionSolutionPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * IntroductionSolutionPanel is the panel titled 'Solution' in the 'Introduction' screen.
--- js/mysolution/view/StringSwitch.ts
+++ js/mysolution/view/StringSwitch.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * StringSwitch is an ABSwitch that switches between string values, and is labeled with strings.
