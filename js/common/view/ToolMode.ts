42479f87fabdbbeb788a18889caa8c9783c99292
pixelzoom <cmalley@pixelzoom.com>
2023-08-03 20:38:31 -0600

    doc, https://github.com/phetsims/acid-base-solutions/issues/206

--- js/common/view/ToolMode.ts
+++ js/common/view/ToolMode.ts
@@ -1,7 +1,8 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
 /**
- * Possible choices for the Tools radio buttons.
+ * Possible choices for the Tools radio buttons. Note that the 'none' value can be selected via PhET-iO use only,
+ * for hiding all tools; there is no way to select 'none' in the user interface.
  *
  * @author Chris Malley (PixelZoom, Inc)
  */
