ae8a491f174a99f50a5cc00ae64b72e378a65ec6
pixelzoom <cmalley@pixelzoom.com>
2023-09-12 12:53:55 -0600

    update QA credits, https://github.com/phetsims/acid-base-solutions/issues/219

--- js/acid-base-solutions-main.ts
+++ js/acid-base-solutions-main.ts
@@ -32,7 +32,7 @@ simLauncher.launch( () => {
       leadDesign: 'Kelly Lancaster',
       softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
       team: 'Bryce Gruneich, Patricia Loeblein, Emily B. Moore, Robert Parson, Kathy Perkins, Amy Rouinfar',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Oliver Orejola, Benjamin Roberts, Bryan Yoelin',
+      qualityAssurance: 'Steele Dalton, Jaron Droder, Bryce Griebenow, Elise Morgan, Oliver Orejola, Benjamin Roberts, Nancy Salpepi, Kathryn Woessner, Bryan Yoelin',
       thanks: '\u2022 Conversion of this simulation to HTML5 was funded in part by the Royal Society of Chemistry.<br>' +
               '\u2022 Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
     },
