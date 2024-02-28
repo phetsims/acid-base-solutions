7068245060a41dde47325911ce9e189185df608c
phet-dev <phetadmin@gmail.com>
2024-02-28 03:11:14 -0700

    update copyright dates from daily grunt work

--- js/intro/view/IntroSolutionPanel.ts
+++ js/intro/view/IntroSolutionPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * IntroSolutionPanel is the panel titled 'Solution' in the 'Intro' screen.
