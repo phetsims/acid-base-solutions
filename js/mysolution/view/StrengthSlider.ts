729515b18e030c3fe6516a49351628fec585c8db
phet-dev <phetadmin@gmail.com>
2024-07-17 03:19:15 -0600

    update copyright dates from daily grunt work

--- js/mysolution/view/StrengthSlider.ts
+++ js/mysolution/view/StrengthSlider.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * StrengthSlider is a logarithmic slider for controlling strength of the solution.
