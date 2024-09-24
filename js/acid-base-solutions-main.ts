afec2d5c244927242f972d7d0ecac1b588fcff57
phet-dev <phetadmin@gmail.com>
2024-09-24 03:16:44 -0600

    update copyright dates from daily grunt work

--- js/acid-base-solutions-main.ts
+++ js/acid-base-solutions-main.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * This is the main entry point for the 'Acid-Base Solutions' simulation.
