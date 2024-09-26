13296ae7ffa230583732213fa1dcdaf3b92919df
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:40 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for acid-base-solutions.
+ * ESLint configuration for acid-base-solutions.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
