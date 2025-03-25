7178a3ee9dd460af59b5f8e144ee61100ca3e133
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:32:50 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/ABSQueryParameters.ts
+++ js/common/ABSQueryParameters.ts
@@ -8,6 +8,7 @@
  */
 
 import logGlobal from '../../../phet-core/js/logGlobal.js';
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import acidBaseSolutions from '../acidBaseSolutions.js';
 
 const SCHEMA_MAP = {
