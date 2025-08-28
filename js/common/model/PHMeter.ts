bef1981621de6d6fa609a9786eeac5f4a5a0b765
phet-dev <phetadmin@gmail.com>
2025-08-28 03:32:00 -0600

    update copyright dates from daily grunt work

--- js/common/model/PHMeter.ts
+++ js/common/model/PHMeter.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * PHMeter is the model of a movable pH meter. Position is at the tip of the probe.
--- js/common/model/solutions/AqueousSolution.ts
+++ js/common/model/solutions/AqueousSolution.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * AqueousSolution is the abstract base class for solutions.
